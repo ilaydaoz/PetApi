175b1422af70d6bd4ebdf9f902f5162305854eb6
ilayda <ilaydaozken@gmail.com>
2023-05-12 16:32:24 +0300

    User Enitiy oluşturuldu

---
+++ Core/Pet.Core.Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using Shared.Pet.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pet.Core.Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public string Name { get; set; }
+        public string NickName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Shared.Pet/Entity/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Pet.Entity
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
