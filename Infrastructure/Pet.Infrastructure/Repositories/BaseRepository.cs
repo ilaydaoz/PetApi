db2347ea88f2b8625204197585de44459ec25357
Sumeyye <sumeyyesabirli@gmail.com>
2023-05-13 00:58:15 +0300

    BaseRepository ve User Repository oluşturuldu

---
+++ Infrastructure/Pet.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore;
+using Pet.Infrastructure.Context;
+using Shared.Pet.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Pet.Core.Application_.Repositories;
+using Shared.Pet.Repositories;
+
+namespace Pet.Infrastructure.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : BaseEntity
+    {
+        private readonly EfDbContext _context;
+
+        public BaseRepository(EfDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> AddAsync(T entity)
+        {
+            EntityEntry<T> entityEntry = await _context.AddAsync(entity);
+            return entityEntry.State == EntityState.Added;
+        }
+
+        public bool Delete(T entity)
+        {
+            EntityEntry<T> entityEntry = _context.Remove(entity);
+            return entityEntry.State == EntityState.Deleted;
+        }
+        public bool DeleteRange(List<T> entity)
+        {
+            _context.RemoveRange(entity);
+            return true;
+        }
+        public IQueryable<T> GetAll()
+        {
+            return _context.Set<T>();
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            return await _context.Set<T>().FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> filter)
+        {
+            return await _context.Set<T>().FirstOrDefaultAsync(filter);
+        }
+
+        public bool Update(T entity)
+        {
+            EntityEntry<T> entityEntry = _context.Update(entity);
+            return entityEntry.State == EntityState.Modified;
+        }
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public IEnumerable<T> GetByFilter(Func<T, bool> filter)
+        {
+            return _context.Set<T>().Where(filter).ToList();
+        }
+
+    }
+}
---
+++ Shared.Pet/Repositories/IBaseRepository.cs
@@ -0,0 +1,24 @@
+﻿using Shared.Pet.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Pet.Repositories
+{
+    public interface IBaseRepository<T> where T : BaseEntity
+    {
+        bool Update(T entity);
+        bool Delete(T entity);
+        bool DeleteRange(List<T> entity);
+        Task<int> SaveAsync();
+        Task<T> GetByIdAsync(Guid id);
+        Task<bool> AddAsync(T entity);
+        IQueryable<T> GetAll();
+        IEnumerable<T> GetByFilter(Func<T, bool> filter);
+        Task<T> GetSingleAsync(Expression<Func<T, bool>> method);
+        //vermiş olduğumuz şarta uygun ilk tekil nesneyi getirir
+    }
+}
