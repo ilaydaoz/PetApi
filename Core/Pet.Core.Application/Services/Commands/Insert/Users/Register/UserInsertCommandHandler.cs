b46de37be6f3c93ddf9df870bdfe3873ed38f74f
ilayda <ilaydaozken@gmail.com>
2023-05-19 23:12:52 +0300

    PasswordHashing yapıldı

--- Core/Pet.Core.Application/Services/Commands/Insert/Users/Register/UserInsertCommandHandler.cs
+++ Core/Pet.Core.Application/Services/Commands/Insert/Users/Register/UserInsertCommandHandler.cs
@@ -2,6 +2,7 @@
 using MediatR;
 using Pet.Core.Application_.Repositories;
 using Pet.Core.Domain.Entities;
+using Shared.Pet.Hashing;
 
 namespace Pet.Core.Application_.Services.Commands.Insert.Users.Register
 {
@@ -16,7 +17,9 @@ namespace Pet.Core.Application_.Services.Commands.Insert.Users.Register
         }
         public async Task<UserInsertCommandResponse> Handle(UserInsertCommandRequestModel request, CancellationToken cancellationToken)
         {
+            string hashedPassword = PasswordHashing.HashingHelper(request.Password, out string salt);
             var user = _mapper.Map<User>(request);
+            user.Password = hashedPassword;
             var userAdd = await _userRepository.InsertAsync(user);
             await _userRepository.SaveAsync();
             var userMap = _mapper.Map<UserInsertCommandResponse>(userAdd);
