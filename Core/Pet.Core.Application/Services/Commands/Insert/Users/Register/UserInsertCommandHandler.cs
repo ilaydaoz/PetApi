89c60d09e076213b5dc2e3be3a772ce49b085c87
ilayda <ilaydaozken@gmail.com>
2023-05-23 21:02:36 +0300

    Validation register yapıldı

--- Core/Pet.Core.Application/Services/Commands/Insert/Users/Register/UserInsertCommandHandler.cs
+++ Core/Pet.Core.Application/Services/Commands/Insert/Users/Register/UserInsertCommandHandler.cs
@@ -1,8 +1,11 @@
 ﻿using AutoMapper;
+using FluentValidation;
 using MediatR;
 using Pet.Core.Application.Repositories;
 using Pet.Core.Domain.Entities;
 using Shared.Pet.Hashing;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Pet.Core.Application.Services.Commands.Insert.Users.Register
 {
@@ -10,19 +13,30 @@ namespace Pet.Core.Application.Services.Commands.Insert.Users.Register
     {
         private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
-        public UserInsertCommandHandler(IUserRepository userRepository, IMapper mapper)
+        private readonly IValidator<UserInsertCommandRequestModel> _validator;
+
+        public UserInsertCommandHandler(IUserRepository userRepository, IMapper mapper, IValidator<UserInsertCommandRequestModel> validator)
         {
             _userRepository = userRepository;
             _mapper = mapper;
+            _validator = validator;
         }
+
         public async Task<UserInsertCommandResponse> Handle(UserInsertCommandRequestModel request, CancellationToken cancellationToken)
         {
             string salt;
             string hashedPassword = PasswordHashing.HashingHelper(request.Password, out salt);
             var user = _mapper.Map<User>(request);
             user.Password = hashedPassword;
-            user.PasswordSalt= salt; 
+            user.PasswordSalt = salt;
             var userAdd = await _userRepository.InsertAsync(user);
+
+            var validationResult = await _validator.ValidateAsync(request);
+            if (!validationResult.IsValid)
+            {
+                throw new ValidationException(validationResult.Errors);
+            }
+
             await _userRepository.SaveAsync();
             var userMap = _mapper.Map<UserInsertCommandResponse>(userAdd);
             return userMap;
